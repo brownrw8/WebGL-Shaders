43ad03ce3d47f11bdb6d131966b28ac02bb24c0d
rbrown <Robert-Brown@idexx.com>
2017-08-09 10:28:25 -0400

    initial commit

---
+++ gulpfile.js
@@ -0,0 +1,34 @@
+'use strict';
+ 
+var gulp = require('gulp4');
+var http = require('http');
+var ecstatic = require('ecstatic');
+var browserSync = require('browser-sync').create();
+var Q = require('q');
+var path = require('path');
+
+const port = "8080";
+const publicDir = "public";
+const indexPage = publicDir + "/index.html";
+
+gulp.task('launch:node', function(){
+    let deferred = Q.defer();
+    http.createServer(
+        ecstatic({ root: path.join(__dirname, publicDir) })
+    ).listen(port);
+    console.log('Started http server on :' + port);
+    deferred.resolve();
+    return deferred.promise;
+});
+
+gulp.task('launch:browser', function () {
+    browserSync.init({
+        proxy: "http://localhost:" + port + "/" + indexPage
+    });
+    gulp.watch("public/**/*").on('change', browserSync.reload);
+});
+
+gulp.task('launch', gulp.series(
+                            'launch:node', 
+                            'launch:browser'
+                    ));
